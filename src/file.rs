567de95112a9f3a8588cbf351755bd09bc3d1e20
dxq <duanxuqiang@ucse.net>
2019-01-18 18:02:46 +0800

    file

--- src/file.rs
+++ src/file.rs
@@ -1,15 +1,32 @@
 use std::fs::File;
 use std::io::{self, Read};
 
-//读取file
-pub fn read_file_data(filename: &str) -> io::Result<String> {
-    let mut text = String::new();
-    File::open(&filename)?.read_to_string(&mut text)?;
-    Ok(text)
-}
+pub struct MyFile;
+impl MyFile {
+    //读取file
+    pub fn read(filename: &str) -> io::Result<String> {
+        let mut text = String::new();
+        File::open(&filename)?.read_to_string(&mut text)?;
+        Ok(text)
+    }
 
-pub fn save_file_data(filename: &str,file_data: &str) -> io::Result<String> {
-    let mut text = String::new();
-    File::open(&filename)?.write(&mut text)?;
-    Ok(text)
+    pub fn save(filename: &str,file_data: &str) -> io::Result<String> {
+        let mut text = String::new();
+        File::open(&filename)?.write(&mut text)?;
+        Ok(text)
+    }
+
+    pub fn add(filename: &str,file_data: &str) -> io::Result<String> {
+        let mut text = String::new();
+        File::open(&filename)?.write(&mut text)?;
+        Ok(text)
+    }
+
+    pub fn log(filename: &str,file_data: &str) -> io::Result<String> {
+        let mut text = String::new();
+        File::open(&filename)?.write(&mut text)?;
+        Ok(text)
+    }
+
+
 }
